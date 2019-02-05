38cd600bc4e78646c6d45935ee42fbd0e019e85c
Kitty <39654422+kat3ng@users.noreply.github.com>
2019-02-04 22:27:33 -0600

    Updates

---
+++ precache-manifest.8b3da5151a82e2eae4d13790f368e5e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d2fc014029aa4af9e67",
+    "url": "/clicky-game/static/css/main.f2d1e878.chunk.css"
+  },
+  {
+    "revision": "1d2fc014029aa4af9e67",
+    "url": "/clicky-game/static/js/main.1d2fc014.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "98425fe6db7ad40842fb25a51f63d8ef",
+    "url": "/clicky-game/index.html"
+  }
+];
